e0826e51dd97cd48ec4598ce832123306a1e8dbf
SourisT2N <70143847+SourisT2N@users.noreply.github.com>
2020-09-22 00:01:44 +0700

    Add files via upload

--- Quản lý sinh viên/exportToExcel.js
+++ Quản lý sinh viên/exportToExcel.js
@@ -1,36 +1,22 @@
-// const exportBtn = document.querySelector(".export-btn");
+const exportBtn = document.querySelector(".export-btn");
+// const date = new Date();
 
-// exportBtn.onclick = () => {
-//     exportToExcel('table1','qlsv');
-// }
+exportBtn.onclick = () => {
+	const trTable  = document.querySelectorAll("#table1 tbody tr");
+	if(trTable.length > 0) {
+	    tableToExcel('table1');
+	}
+	else alert("Chưa Có Dữ Liệu Để Xuất");
+}
 
-// function exportToExcel(tableID, filename = ''){
-//     var downloadurl;
-//     var dataFileType = 'application/vnd.ms-excel';
-//     var tableSelect = document.getElementById(tableID);
-//     var tableHTMLData = tableSelect.outerHTML.replace(/ /g, '%20');
-    
-//     // Specify file name
-//     filename = filename?filename+'.xls':'export_excel_data.xls';
-    
-//     // Create download link element
-//     downloadurl = document.createElement("a");
-    
-//     document.body.appendChild(downloadurl);
-    
-//     if(navigator.msSaveOrOpenBlob){
-//         var blob = new Blob(['\ufeff', tableHTMLData], {
-//             type: dataFileType
-//         });
-//         navigator.msSaveOrOpenBlob( blob, filename);
-//     }else{
-//         // Create a link to the file
-//         downloadurl.href = 'data:' + dataFileType + ', ' + tableHTMLData;
-    
-//         // Setting the file name
-//         downloadurl.download = filename;
-        
-//         //triggering the function
-//         downloadurl.click();
-//     }
-// }
+var tableToExcel = (function() {
+	var uri = 'data:application/vnd.ms-excel;base64,'
+   , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><meta http-equiv="content-type" content="application/vnd.ms-excel; charset=UTF-8"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
+   , base64 = function(s) { return window.btoa(unescape(encodeURIComponent(s))) }
+   , format = function(s, c) { return s.replace(/{(\w+)}/g, function(m, p) { return c[p]; }) }
+ return function(table, name) {
+   if (!table.nodeType) table = document.getElementById(table);
+   var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
+   window.location.href = uri + base64(format(template, ctx))
+ }
+})()
---
+++ Quản lý sinh viên/selectRandom.js
@@ -0,0 +1,134 @@
+const students = [
+	{
+		name: 'Bùi Linh Chi',
+	},
+	{
+		name: 'Trịnh Thị Ái Phi',
+	},
+	{
+		name: 'Trần Nam Phương',
+	},
+	{
+		name: 'Đỗ Hồng Hạnh',
+	},
+	{
+		name: 'Nguyễn Hà My',
+	},
+	{
+		name: 'Phạm Nguyễn Bảo Ngọc',
+	},
+	{
+		name: 'Nguyễn Thị Mai Hương',
+	},
+	{
+		name: 'Trần Quốc Tuấn',
+	},
+	{
+		name: 'Trịnh Hoàng',
+	},
+	{
+		name: 'Lý Thiện',
+	},
+	{
+		name: 'Lý Hải',
+	},
+	{
+		name: 'Bùi Thị Huệ',
+	},
+	{
+		name: 'Nguyễn Chí Linh',
+	},
+	{
+		name: 'Trần Văn Công',
+	},
+	{
+		name: 'Lý Kiệt',
+	},
+]
+
+const addressStudents = ['Mạc Đỉnh Chi','Ngô Gia Tự','Nguyễn Trãi','Nguyễn Huệ','Quang Trung',
+'Nguyễn Tất Thành','Ngô Sỹ Liên','Phù Đổng','Trần Nhật Duật','Lê Hồng Phong','Lê Lợi','Trần Phú',
+'Trịnh Phong','Đinh Tiên Hoàng','Đinh Bộ Lĩnh'];
+
+const selectBtn = document.querySelector(".select-btn");
+const selectOption = document.getElementById("test");
+
+selectBtn.onclick = () => {
+	const selectValue = selectOption.value;
+	let id;
+	for(let i = 0;i < students.length;i++){
+		let numberAddress = Math.floor(Math.random()*500 + 1);
+		let addressStudent = addressStudents[Math.floor(Math.random()*addressStudents.length)];
+		students[i].address = `${numberAddress} ${addressStudent}`;
+	}
+	if(selectValue == 5) {
+		const obj = randomSelectOption(selectValue);
+		for(let i = 0;i < obj.length;i++){
+			id = new Date().getTime() + Math.floor(Math.random()*100);
+			createNewElement(obj[i],id);
+			addLocalStorage(obj[i],id);
+			arr.push(obj[i]);
+		}
+	}
+	else if(selectValue == 10) {
+		const obj = randomSelectOption(selectValue);
+		for(let i = 0;i < obj.length;i++){
+			id = new Date().getTime() + Math.floor(Math.random()*100);
+			createNewElement(obj[i],id);
+			addLocalStorage(obj[i],id);
+			arr.push(obj[i]);
+		}
+	}
+	else if(selectValue == 15) {
+		const obj = randomSelectOption(selectValue);
+		for(let i = 0;i < obj.length;i++){
+			id = new Date().getTime() + Math.floor(Math.random()*101);
+			createNewElement(obj[i],id);
+			addLocalStorage(obj[i],id);
+			arr.push(obj[i]);
+		}
+	}
+}
+
+function randomSelectOption (value) {
+	let mid;
+	let codes = '';
+	let arrStudents = [];
+	const studentsLength = students.length;
+	for(let i = 0;i < studentsLength;i++){
+		let j = Math.floor(Math.random()*(studentsLength - 1));
+		mid = students[i];
+		students[i] = students[j];
+		students[j] = mid;
+		if(arrStudents.length > 0 && arrStudents.length < Number(value)){
+			const checkStudents = arrStudents.some(item => item.name.includes(students[i].name)||item.address.includes(students[i].address));
+			if(!checkStudents) {
+				arrStudents.push(students[i]);
+			}
+		}
+		else if(arrStudents.length == 0) {arrStudents.push(students[i]);}
+		if(arrStudents.length >= Number(value)){
+			break;
+		}
+	}
+	const lengthStudents = arrStudents.length;
+	let countCode = 0;
+	const arrCode = [1,2,3,4,5,6,7,8,9]
+	for(let i = 0;i < students.length;i++){
+		for(let j = 1;j <= 8;j++){
+			codes += Math.floor(Math.random()*9 + 1);
+		}
+		if(arrStudents[i].code == codes){
+			continue;
+		}
+		else {
+			arrStudents[i].code = codes;
+			countCode++;
+		}
+		codes = '';
+		if(countCode == lengthStudents){
+			break;
+		}
+	}
+	return arrStudents;
+}
